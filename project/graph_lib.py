365ec55a6c86ed3eb3359255f3e5f22b7c7f87ca
f1i3g3 <dmitrvlkuznetsov@gmail.com>
2022-09-14 23:51:46 +0300

    Redid and added more tests

--- project/graph_lib.py
+++ project/graph_lib.py
@@ -1,16 +1,43 @@
 import string
+from typing import Iterable
+
 import cfpq_data
 import networkx
+import networkx as nx
 
 
-def get_graph_info(gr_name: string):
-    graph = cfpq_data.graph_from_csv(cfpq_data.download(gr_name))
+class GraphInformation:
+    nodes: int
+    edges: int
+    labels: Iterable[str]
+
+    def __init__(self, nodes, edges, labels):
+        self.nodes = nodes
+        self.edges = edges
+        self.labels = labels
+
+    def __eq__(self, other):
+        result = (
+            isinstance(other, GraphInformation)
+            and self.nodes == other.nodes
+            and self.edges == other.edges
+            and self.labels == other.labels
+        )
+
+        return result
+
+
+def get_graph_info(load_flag: bool, graph_param):
+    if load_flag:
+        graph = cfpq_data.graph_from_csv(cfpq_data.download(graph_param))
+    else:
+        graph = graph_param
 
     nodes_num = graph.number_of_nodes()
     edges_num = graph.number_of_edges()
     labels = {i[2]["label"] for i in graph.edges.data(default=True)}
 
-    return nodes_num, edges_num, labels
+    return GraphInformation(nodes_num, edges_num, labels)
 
 
 def generate_and_write_two_cycles_graph(n, m, labels, path):
--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -1,46 +1,71 @@
 import filecmp
 import os
+import pytest
+import networkx
 
 from project.graph_lib import *
 
 
 def test_get_graph_info():
-    (nodes, edges, labels) = get_graph_info("people")
+    actual_info = get_graph_info(True, "people")
+    expected_info = GraphInformation(
+        377,
+        640,
+        {
+            "allValuesFrom",
+            "comment",
+            "complementOf",
+            "disjointWith",
+            "domain",
+            "drives",
+            "equivalentClass",
+            "first",
+            "has_pet",
+            "intersectionOf",
+            "inverseOf",
+            "is_pet_of",
+            "label",
+            "maxCardinality",
+            "minCardinality",
+            "onProperty",
+            "range",
+            "reads",
+            "rest",
+            "service_number",
+            "someValuesFrom",
+            "subClassOf",
+            "subPropertyOf",
+            "type",
+            "unionOf",
+        },
+    )
 
-    # from: https: // jetbrains - research.github.io / CFPQ_Data / dataset / index.html
-    assert nodes == 337
-    assert edges == 640
-    assert labels == {
-        "allValuesFrom",
-        "comment",
-        "complementOf",
-        "disjointWith",
-        "domain",
-        "drives",
-        "equivalentClass",
-        "first",
-        "has_pet",
-        "intersectionOf",
-        "inverseOf",
-        "is_pet_of",
-        "label",
-        "maxCardinality",
-        "minCardinality",
-        "onProperty",
-        "range",
-        "reads",
-        "rest",
-        "service_number",
-        "someValuesFrom",
-        "subClassOf",
-        "subPropertyOf",
-        "type",
-        "unionOf",
-    }
+    assert actual_info == expected_info
 
 
 def test_generate_two_cycles_graph():
     generate_and_write_two_cycles_graph(3, 2, ["x", "y"], "tests/files/generated.dot")
 
     assert filecmp.cmp("tests/files/generated.dot", "tests/files/expected.dot")
+    assert networkx.is_isomorphic()
+
     os.remove("tests/files/generated.dot")
+
+
+def test_generate_graph_without_cycles():
+    with pytest.raises(Exception):
+        generate_and_write_two_cycles_graph(
+            (0, 0), ("x", "y"), "tests/files/generated.dot"
+        )
+
+
+def test_no_name_graph():
+    with pytest.raises(Exception):
+        get_graph_info(True, "this_name_surely_does_not_exist")
+
+
+def test_empty_graph():
+    actual_info = get_graph_info(False, networkx.empty_graph())
+    expected_info = GraphInformation(0, 0, iter())
+
+    assert expected_info == actual_info

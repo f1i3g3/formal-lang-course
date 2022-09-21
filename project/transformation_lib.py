72b6c7453c5589f8b5323fe897081011d0ff6ba9
f1i3g3 <dmitrvlkuznetsov@gmail.com>
2022-09-21 23:28:27 +0300

    Added task 2

---
+++ project/transformation_lib.py
@@ -0,0 +1,39 @@
+from pyformlang.regular_expression import Regex
+from pyformlang.finite_automaton import (
+    DeterministicFiniteAutomaton,
+    NondeterministicFiniteAutomaton,
+)
+
+import networkx
+
+
+def regex_to_dfa(rx):
+    return Regex(rx).to_epsilon_nfa().minimize()
+
+
+def graph_to_dfa(
+    initial_graph, start_states: set | None = None, final_states: set | None = None
+):
+    final_graph = NondeterministicFiniteAutomaton()
+    final_graph.add_transitions(
+        [x, l, y] for x, y, l in initial_graph.edges(data="label") if l
+    )
+
+    nodes = initial_graph.nodes
+
+    if not start_states:
+        start_states = nodes
+    elif not start_states.issubset(nodes):
+        raise Exception("Error!")
+
+    if not final_states_states:
+        final_states = nodes
+    elif not final_states.issubset(nodes):
+        raise Exception("Error!")
+
+    for s in start_states:
+        final_states.add_start_state(s)
+    for s in final_states:
+        final_states.add_finish_state(s)
+
+    return final_states
---
+++ tests/test_transformation_dfa.py
@@ -0,0 +1,48 @@
+from project.transformation_lib import *
+
+
+def test_nonexistent_start():
+    graph = nx.MultiDiGraph()
+    graph.add_edge(0, 0, label="1")
+    graph.add_edge(0, 1, label="0")
+    graph.add_edge(1, 1, label="0")
+    graph.add_edge(1, 0, label="1")
+    with pytest.raises(Exception, match=".* start .*"):
+        graph_to_nfa(graph, {42}, {0})
+    with pytest.raises(Exception, match=".* final .*"):
+        graph_to_nfa(graph, {0}, {42})
+
+
+def test_ended_by_zero():
+    graph = nx.MultiDiGraph()
+    graph.add_edge(0, 0, label="1")
+    graph.add_edge(0, 1, label="0")
+    graph.add_edge(1, 1, label="0")
+    graph.add_edge(1, 0, label="1")
+    nfa = graph_to_nfa(graph, {0}, {1})
+    assert nfa.is_equivalent_to(regex_str_to_dfa("(0|1)* 0"))
+    for i in range(0, 42, 2):
+        assert nfa.accepts("{0:b}".format(i))
+        assert not nfa.accepts("{0:b}".format(i + 1))
+
+
+def test_binary_ended_by_banana_ananas():
+    graph = nx.MultiDiGraph()
+    for i in range(ord("a"), ord("z") + 1):
+        graph.add_edge(0, 0, label=chr(i))
+    graph.add_edge(0, 0, label=" ")
+    graph.add_edge(0, 1, label="b")
+    graph.add_edge(0, 2, label="a")
+    graph.add_edge(1, 2, label="a")
+    graph.add_edge(2, 3, label="n")
+    graph.add_edge(3, 4, label="a")
+    graph.add_edge(4, 5, label="n")
+    graph.add_edge(5, 6, label="a")
+    graph.add_edge(6, 7, label="s")
+
+    nfa = graph_to_nfa(graph, {0}, {6, 7})
+
+    assert nfa.accepts("banana")
+    assert nfa.accepts("ananas")
+    assert nfa.accepts("bananas")
+    assert not nfa.accepts("apple")

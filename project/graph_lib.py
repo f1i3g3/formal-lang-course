f20e6f09f58eac273d24610aba0634ab5dcaf27b
Dmitri Kuznetsov <dmitrvlkuznetsov@gmail.com>
2025-09-12 23:39:59 +0300

    feat: added task 2 implementation & some tests

--- project/graph_lib.py
+++ project/graph_lib.py
@@ -2,6 +2,9 @@ from typing import Set
 
 import cfpq_data
 import networkx
+from networkx import MultiDiGraph
+from pyformlang.finite_automaton import DeterministicFiniteAutomaton, NondeterministicFiniteAutomaton
+from pyformlang.regular_expression import Regex
 
 
 class GraphInformation:
@@ -50,3 +53,47 @@ def generate_and_write_two_cycles_graph(num_first, num_second, labels, path):
     graph = cfpq_data.labeled_two_cycles_graph(num_first, num_second, labels=labels)
 
     networkx.nx_pydot.write_dot(graph, path)
+
+
+def regex_to_dfa(regex: str) -> DeterministicFiniteAutomaton:
+    """
+
+    """
+    return Regex(regex).to_epsilon_nfa().minimize()
+
+
+def graph_to_nfa(
+        graph: MultiDiGraph, start_states: Set[int] = None, final_states: Set[int] = None
+) -> NondeterministicFiniteAutomaton:
+    """
+
+    """
+    result_graph = NondeterministicFiniteAutomaton()
+    result_graph.add_transitions(
+        [(x, l, y) for x, y, l in graph.edges(data="label") if l]
+    )
+
+    # TODO: documentation!!
+
+    nodes = graph.nodes()
+
+    if not start_states:
+        start_states = nodes
+    elif not start_states.issubset(nodes):
+        raise Exception(
+            f"Error! Initial graph does not contain start states: {start_states.difference(set(nodes))}."
+        )
+
+    if not final_states:
+        final_states = nodes
+    elif not final_states.issubset(nodes):
+        raise Exception(
+            f"Error! Initial graph does not contain final states: {final_states.difference(set(nodes))}."
+        )
+
+    for s in start_states:
+        result_graph.add_start_state(s)
+    for s in final_states:
+        result_graph.add_final_state(s)
+
+    return result_graph
